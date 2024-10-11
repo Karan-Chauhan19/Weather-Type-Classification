28e9015bbc2e8998ff949480959841f28b22e726
karan190806 <kc879022@gmail.com>
2024-10-11 12:08:55 +0530

    test.py

--- src/test.py
+++ src/test.py
@@ -11,9 +11,9 @@ class TestDataPreprocessing :
         user_input_transform = model_pipeline.named_steps['preprocessor'].transform(user_df)
         user_prediction = model_pipeline.named_steps['classifier'].predict(user_input_transform)
 
-        print(classification_report(y_test,user_prediction))
+        print("Current weather as per your input is",''.join(user_prediction))
 
 test = TestDataPreprocessing()
-test.testing({'Temperature':[38], 'Humidity':[83], 'Wind_Speed':[7], 'Precipitation (%)':[82],
-       'Cloud_Cover':['clear'], 'Atmospheric_Pressure':[1026.5], 'UV_Index':[7], 'Season':['Spring'],
-       'Visibility (km)':[1], 'Location':['coastal']})
+test.testing({'Temperature':[33], 'Humidity':[45], 'Wind_Speed':[8], 'Precipitation (%)':[90],
+       'Cloud_Cover':['cloudy'], 'Atmospheric_Pressure':[1010], 'UV_Index':[5], 'Season':['Autumn'],
+       'Visibility (km)':[10], 'Location':['inland']})
