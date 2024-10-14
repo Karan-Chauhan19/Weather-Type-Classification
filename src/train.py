72dda4f1bbe4a27bfdf97483e3575953bbe270d3
karan190806 <kc879022@gmail.com>
2024-10-14 22:02:10 +0530

    change

--- src/train.py
+++ src/train.py
@@ -17,7 +17,7 @@ from sklearn.pipeline import Pipeline
 
 data = Featureengineering()
 df,preprocessor = data.get_clean_data()
-X = df.drop(columns=['WeatherType'])
+X = df.drop(columns=['WeatherType','Location'])
 y = df.iloc[:,-1]
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
