b3d1188cc99937f54572203b1c54abe1596acf00
karan190806 <kc879022@gmail.com>
2024-10-20 14:53:04 +0530

    add some other algorithm

--- src/train.py
+++ src/train.py
@@ -13,17 +13,48 @@ from sklearn.ensemble import RandomForestClassifier,BaggingClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report,accuracy_score
 from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+import streamlit as st
 
 
-data = Featureengineering()
-df,preprocessor = data.get_clean_data()
-X = df.drop(columns=['WeatherType','Location'])
-y = df.iloc[:,-1]
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
 
-model_pipeline = Pipeline(steps=[
-    ('preprocessor',preprocessor),
-    ('classifier',RandomForestClassifier(n_estimators=20,max_samples=0.5,max_features=0.6,max_depth=8))
-])
 
-model_pipeline.fit(X_train,y_train)
+class ModelTrain :
+    def __init__(self) :
+        pass
+
+    
+
+    def train_model(self) :
+
+        data = Featureengineering()
+        df,preprocessor = data.get_clean_data()
+        X = df.drop(columns=['WeatherType','Location'])
+        y = df.iloc[:,-1]
+        # X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+
+        model_pipeline = Pipeline(steps=[
+            ('preprocessor',preprocessor)
+        ])
+        model_pipeline.fit(X,y)
+        transform_data = model_pipeline.named_steps['preprocessor'].transform(X)
+        options = ['Random Forest Classifier','Decision Tree','Logistic Regression','SVM']
+        selected_option = st.sidebar.selectbox("Select Algorithm ",options)
+
+        Selected_Algorithm = object
+
+        if selected_option == 'Random Forest Classifier' :
+            Selected_Algorithm = RandomForestClassifier()
+            Selected_Algorithm.fit(transform_data,y)
+        elif selected_option == 'Decision Tree' :
+            Selected_Algorithm = DecisionTreeClassifier()
+            Selected_Algorithm.fit(transform_data,y)
+        elif selected_option == 'Logistic Regression' :
+            Selected_Algorithm = LogisticRegression()
+            Selected_Algorithm.fit(transform_data,y)
+        else :
+            Selected_Algorithm = SVC()
+            Selected_Algorithm.fit(transform_data,y)
+
+        return Selected_Algorithm,model_pipeline
