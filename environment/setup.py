7e9bc71688917bc7c660bbb58e15f1f9b2c3d774
Karan Chauhan <kc879022@gmail.com>
2025-03-27 11:41:24 +0530

    added setup.py file

---
+++ environment/setup.py
@@ -0,0 +1,19 @@
+'''
+author: Karan Chauhan
+github: @Karan-Chauhan19
+organization: L.J University
+'''
+from setuptools import  setup, find_packages
+
+setup(
+    name='Project',  # project name
+    version='1.0',  # version number
+    description='A Synthetic Weather Classification Project',  # description of the project
+    packages = find_packages(),  # find all packages
+    author='Karan-Chauhan' ,# author of the package
+    author_email='kc879022@gmail.com', # email of the author
+    url='https://github.com/karan190806/Project.git', # url of the project
+    install_requires=['pandas', 'numpy', 'sklearn', 'matplotlib'],
+    # list of the dependencies required by the package
+    classifiers=['Programining Language :: python :: 3.12.3']
+)
